47b0faeb6d8e6a0ebae0d6be41adb491246a05c7
r2abreu <r2abreu@pm.me>
2021-01-22 13:24:14 +0100

    Plug youtube api

--- src/api/youtube.ts
+++ src/api/youtube.ts
@@ -1,9 +1,6 @@
 import axios from 'axios';
 const KEY = process.env.VUE_APP_YOTUBE_API;
 
-
-console.log(KEY)
-
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3/search',
     params: {
